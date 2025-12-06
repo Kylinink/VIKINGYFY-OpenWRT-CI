d3a44ee460c7041ff25a1ca3f47fbcfae7eb1820
Kylin <37763338+Kylinink@users.noreply.github.com>
2025-12-06 16:42:28 +0800

    Add new packages to Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -73,6 +73,9 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
+UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
