d06b09e4da3f3cebce13eebd1759c9c6f9d96801
Kylin <37763338+Kylinink@users.noreply.github.com>
2026-06-09 19:32:52 +0800

    更新 Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -88,6 +88,7 @@ UPDATE_PACKAGE "luci-app-podman" "Zerogiven-OpenWRT-Packages/luci-app-podman" "m
 UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf"
 UPDATE_PACKAGE "openwrt-bandix-plus" "timsaya/openwrt-bandix-plus" "main" "openwrt-bandix-plus"
 UPDATE_PACKAGE "luci-app-bandix-plus" "timsaya/luci-app-bandix-plus" "main" "luci-app-bandix-plus"
+UPDATE_PACKAGE "luci-app-airoha-npu" "bingoguo93/luci-app-airoha-npu" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
