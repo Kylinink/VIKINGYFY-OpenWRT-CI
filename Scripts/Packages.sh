9c68d0afeacfe7692c03963f0f51fd8bb15ba48d
Kylin <37763338+Kylinink@users.noreply.github.com>
2026-07-14 22:22:30 +0800

    Add files via upload

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,6 +82,16 @@ UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "helloworld" "fw876/helloworld" "dev" "" "dns2tcp dnsproxy luci-app-ssr-plus mihomo simple-obfs xray-core"
+UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
+UPDATE_PACKAGE "luci-app-podman" "Zerogiven-OpenWRT-Packages/luci-app-podman" "main"
+UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf"
+UPDATE_PACKAGE "openwrt-bandix-plus" "timsaya/openwrt-bandix-plus" "main" "openwrt-bandix-plus"
+UPDATE_PACKAGE "luci-app-bandix-plus" "timsaya/luci-app-bandix-plus" "main" "luci-app-bandix-plus"
+UPDATE_PACKAGE "luci-app-airoha-npu" "bingoguo93/luci-app-airoha-npu" "main"
+UPDATE_PACKAGE "luci-app-easymesh" "kenzok8/openwrt-packagest" "master" "luci-app-easymesh" "luci-app-easymesh"
+UPDATE_PACKAGE "small-package" "kenzok8/small-package" "main" "daed-next luci-app-daed-next gost luci-app-gost luci-theme-tomato" "daed-next luci-app-daed-next gost luci-app-gost luci-theme-tomato"
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
@@ -130,3 +140,4 @@ UPDATE_VERSION "sing-box"
 if [ -f "$GITHUB_WORKSPACE/Scripts/PRIVATE.sh" ]; then
 	source "$GITHUB_WORKSPACE/Scripts/PRIVATE.sh"
 fi
+
